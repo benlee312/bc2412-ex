5b4006dd21da8ea6019c4c9a8ca51c49dc9a2da2
ben.lee <benlee95312@gmail.com>
2024-12-16 10:04:59 +0800

    [update]

--- JavaQuest6.java
+++ JavaQuest6.java
@@ -8,15 +8,18 @@ public class JavaQuest6 {
    */
   public static void main(String[] args) {
     int first = 0, second = 1;
-    int sum = 0;
+    int [] sum = new int [15];
     // for loop to print first 15 numbers in Fibonacci Sequence
     for (int i = 0; i < 15; i++){
-      if ( i == 0) {
-        System.out.println(first);
-              }else if (i >= 0){
-
-              }
-              System.out.println(i);
+      if (i == 0){
+        sum [0] = first;
+      } else if (i == 1){
+        sum [1] = second;
+      }
+        else if ( i >= 2) {
+        sum [i] = sum [i - 2] + sum [i - 1];
+      }
+      System.out.println(sum[i]);
     }
     
   }
